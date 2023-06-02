d6ddcbee4f1881bbee62dd21cfd849b91f1075df
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-06-02 16:52:25 +0100

    upadted log config

--- config/winston.config.js
+++ config/winston.config.js
@@ -3,7 +3,10 @@ const winston = require('winston');
 // Create a logger with desired configuration
 const logger = winston.createLogger({
     level: winston.config.syslog.levels, // Set the log level
-    format: winston.format.simple(), // Specify log message format
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ), // Specify log message format
     transports: [
         new winston.transports.Console(), // Log to the console
         new winston.transports.File({ filename: 'logfile.log' }) // Log to a file
@@ -17,19 +20,12 @@ const consoleTransport = new winston.transports.Console({
     format: winston.format.simple(),
 });
 
-// Enable logs to console only in development
-if (process.env.NODE_ENV !== "production") logger.add(consoleTransport);
+logger.add(consoleTransport);
 
 logger.stream = {
     write: function (message) {
         logger.info(message);
     }
 }
-
-// // Log messages at different levels
-// logger.error('An error occurred');
-// logger.warn('A warning message');
-// logger.info('An informational message');
-// logger.debug('A debug message');
 
 module.exports = logger;
