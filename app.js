a2725fbe0844686c8727325c34a7495f0245fe44
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-06-02 19:12:37 +0100

    use token routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { connectDB } = require('./config/db.config');
 const mongoose = require("mongoose");
 const auth = require("./routes/auth.routes")
 const question = require("./routes/question.routes")
+const token = require("./routes/token.routes")
 const logger = require("./config/winston.config")
 const errorMiddlewares = require("./middlewares/error.middleware");
 
@@ -22,6 +23,7 @@ app.use(errorMiddlewares.errorHandler);
 
 app.use('/api/auth', auth)
 app.use('/api/questions', question)
+app.use('/api/token', token)
 
 app.use('/', (req, res) => {
     res.status(404).json(errorMiddlewares.formatError("Resource Not Found"))
