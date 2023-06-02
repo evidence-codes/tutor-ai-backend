93f74ec875e2fffaddd0076c3dc93bdb326e18a0
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-06-02 16:20:07 +0100

    error loggers middleware

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,24 @@
+const { BaseError } = require("../errors/httpErrors");
+const customLogger = require("../config/winston.config");
+
+// eslint-disable-next-line no-unused-vars
+function errorHandler(err, req, res, next) {
+    if (err instanceof BaseError)
+        return res.status(err.statusCode).json(formatError(err.message));
+
+    res.status(500).json(formatError("Internal Server Error"));
+}
+
+function errorLogger(err, req, res, next) {
+    // Only log operational error not HTTP errors
+    if (err instanceof BaseError) return next(err);
+
+    customLogger.error(err.message, err);
+    next(err);
+}
+
+function formatError(message) {
+    return { message, sucess: false };
+}
+
+module.exports = { errorHandler, errorLogger, formatError };
