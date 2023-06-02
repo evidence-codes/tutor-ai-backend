e682948c4b9e2c7393c5860937aaef7dfd0bdbbd
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-06-02 19:52:33 +0100

    change user account details

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,17 @@
 const User = require("../models/user.model")
+const bcrypt = require("bcrypt")
 
 const update = async (req, res) => {
-    if (req.body.password) {
+    const salt = await bcrypt.genSalt();
 
+    if (req.body.password) {
+        req.body.password = await bcrypt.hash(req.body.password, salt);
     }
+
+    const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, { new: true })
+    res.status(200).json(user)
 }
+
+module.exports = { update }
