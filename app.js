6c105a78625814fca977bd073a7a73cc7cec5d03
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-06-02 15:48:58 +0100

    handle data logging

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express()
 const dotenv = require("dotenv")
+const morgan = require("morgan")
 dotenv.config()
 const { connectDB } = require('./config/db.config');
 const mongoose = require("mongoose");
 const auth = require("./routes/auth.routes")
 const question = require("./routes/question.routes")
+const logger = require("./config/winston.config")
+
 
 const port = process.env.PORT || 5000
 const uri = process.env.MONGO_URI
 
 app.use(express.json())
+app.use(morgan("combined", { stream: logger.stream }))
 
 app.use('/api/auth', auth)
 app.use('/api/questions', question)
