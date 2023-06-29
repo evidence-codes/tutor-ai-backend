a6855e834fbee042b4d483771e403c1b5d357384
Akindeju <akindeju@MacBook-Pro.local>
2023-06-28 22:50:10 -0700

    Initial Level Fix

--- controllers/scores.controller.js
+++ controllers/scores.controller.js
@@ -4,18 +4,14 @@ const User = require('../models/user.model');
 const updateLevel = async (req, res) => {
     try {
         const { level } = req.body;
-
         const user = await User.findById(req.params.id);
         if (!user) throw new ResourceNotFound('User does not exist');
-
-        user.level = level;
 
         if (user.level === null) {
             user.initialLevel = level;
         }
-
+        user.level = level;
         await user.save();
-
         res.status(200).json('User Level updated successfully');
     } catch (err) {
         res.status(500).json(err?.message || 'An Error Occured!');
