fc7fcea70a0671e490094bf33a75fcd84800dc6e
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-07-03 08:11:34 +0100

    add user completed lessons function

--- controllers/scores.controller.js
+++ controllers/scores.controller.js
@@ -1,21 +1,36 @@
 const { ResourceNotFound } = require('../errors/httpErrors');
 const User = require('../models/user.model');
 
-const updateLevel = async (req, res) => {
-    try {
-        const { level } = req.body;
+// const updateLevel = async (req, res) => {
+//     try {
+//         const { level } = req.body;
 
-        const user = await User.findById(req.params.id);
+//         const user = await User.findById(req.params.id);
+//         if (!user) throw new ResourceNotFound('User does not exist');
+
+//         user.level = level;
+
+//         await user.save();
+
+//         res.status(200).json('User Level updated successfully');
+//     } catch (err) {
+//         res.status(500).json(err?.message || 'An Error Occured!');
+//     }
+// };
+
+const completedLessons = async (req, res) => {
+    try {
+        const { lessonId, score } = req.body;
+        const user = await User.findById(req.user._id)
         if (!user) throw new ResourceNotFound('User does not exist');
 
-        user.level = level;
-
+        user.lessons = { lessonId, score }
         await user.save();
 
-        res.status(200).json('User Level updated successfully');
+        res.status(200).json('Lesson added to Array')
     } catch (err) {
-        res.status(500).json(err?.message || 'An Error Occured!');
+        res.status(500).json(err)
     }
-};
+}
 
-module.exports = { updateLevel };
+module.exports = { completedLessons };
