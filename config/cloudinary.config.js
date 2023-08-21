991cc1739de9332769d67da159adb88b9af043da
Akindeju <akindeju@MacBook-Pro.local>
2023-08-20 20:59:27 -0700

    Cloudinary Fix

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 cloudinary.config({
-    CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+    cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API,
     api_secret: process.env.CLOUDINARY_SECRET,
     secure: true,
