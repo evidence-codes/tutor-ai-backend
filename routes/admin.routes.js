f3c035a483c062cf468a1e5e7e1fd328baafc0b7
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-09-01 04:12:28 +0100

    admin login route

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { create } = require('../models/admin.model');
+const { create, login } = require('../controllers/admin.controller');
 
 router.post('/create-admin', create)
+
+router.post('/admin-login', login)
 
 module.exports = router;
