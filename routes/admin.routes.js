38a0ff09738b482c907c5f272bf35ddda305d6ac
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-09-07 07:57:55 +0100

    added new routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const { create, login } = require('../controllers/admin.controller');
+const { create, login, noOfUser, subscribedUsers, getAllUsers, newSignup, subscribers } = require('../controllers/admin.controller');
 
 router.post('/create-admin', create)
 
 router.post('/admin-login', login)
+
+router.get('/get-users', noOfUser);
+
+router.get('/get-subscribed-users', subscribedUsers)
+
+router.get('/get-all-users', getAllUsers)
+
+router.get('/new-signup', newSignup);
+
+router.get('/subscribers', subscribers);
+
+
 
 module.exports = router;
