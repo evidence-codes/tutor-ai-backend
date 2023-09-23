8b7aa69c453646d2de7bc19dec5ddcb491f81c66
Akindeju <akindeju@MacBook-Pro.local>
2023-09-23 06:56:56 -0700

    faqs and payment methods

--- routes/admin_var.routes.js
+++ routes/admin_var.routes.js
@@ -2,10 +2,17 @@ const router = require('express').Router();
 const {
     setAdminVariables,
     getAdminVariables,
+    getOnlyAdminVars,
+    setPaymentMethodAdminVariables,
 } = require('../controllers/admin_var.controller');
+const auth = require('../middlewares/auth.middleware');
 
-router.post('/set-variables', setAdminVariables);
+router.patch('/set-variables', auth, setAdminVariables);
+
+router.patch('/set-pm-variables', auth, setPaymentMethodAdminVariables);
 
 router.get('/get-variables', getAdminVariables);
+
+router.get('/get-only-admin-vars', getOnlyAdminVars);
 
 module.exports = router;
--- routes/faq.routes.js
+++ routes/faq.routes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
-const { getFAQs } = require('../controllers/faq.controller');
+const {
+    getFAQs,
+    updateFAQ,
+    deleteFAQs,
+    createFAQ,
+} = require('../controllers/faq.controller');
+const auth = require('../middlewares/auth.middleware');
 
 router.get('/get-faqs', getFAQs);
+
+router.post('/create-faq', auth, createFAQ);
+
+router.patch('/update-faq', auth, updateFAQ);
+
+router.patch('/delete-faqs', auth, deleteFAQs);
 
 module.exports = router;
